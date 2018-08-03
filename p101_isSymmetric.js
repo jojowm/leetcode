a291f014aeb87bcd2adf542d4c47374bbfe7a5c7
weimin <weimin19940508@126.com>
2018-08-03 23:12:59 +0800

    p101

---
+++ p101_isSymmetric.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (!root) {
+        return true;
+    }
+    var Symmetric = function(p, q) {
+        if (p == null && q != null) {
+            return false;
+        }
+        if (p != null && q == null) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return Symmetric(p.left, q.right) && Symmetric(p.right, q.left);
+    };
+    return Symmetric(root.left, root.right);
+};
