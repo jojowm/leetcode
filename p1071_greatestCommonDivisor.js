58e45766ebfdcedccf7744531135ffd1d7cca714
kayleeWei <weimin19940508@126.com>
2020-03-12 13:52:19 +0800

    add: 最大公约数gdc

---
+++ p1071_greatestCommonDivisor.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+  if (str1 + str2 !== str2 + str1) return ''
+  const gcd = (a, b) => (0 === b ? a : gcd(b, a % b))
+  return str1.substring(0, gcd(str1.length, str2.length))
+};
+
+// console.log(gcdOfStrings('ABCABC', 'ABC'))
+// console.log(gcdOfStrings('ABABAB', 'ABAB'))
+console.log(gcdOfStrings('OBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNO', 'OBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNOOBCNO'))
